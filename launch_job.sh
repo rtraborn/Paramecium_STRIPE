e50a870154eb93e0f32b30c6df7734d33f6e3a8e
rtraborn <rtraborn@asu.edu>
2019-10-09 01:43:17 -0400

    Created new branch and added xsymlinks, updated xdoit and launch_job.sh.

--- launch_job.sh
+++ launch_job.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-#PBS -N PnSTRIPE_job
+#PBS -N PpentSTRIPE_job
 #PBS -k o
 #PBS -l nodes=1:ppn=16,vmem=32gb
-#PBS -l walltime=1:00:00
-#PBS -q debug
+#PBS -l walltime=6:00:00
 
 module load java
 
 echo "Launching job"
 
-cd /N/u/rtraborn/Carbonate/ParameciumPromoters/stripeSeq/PnSTRIPE
+cd /N/u/rtraborn/Carbonate/ParameciumPromoters/stripeSeq/PpentSTRIPE
 ./xdoit > err
 
 echo "Job complete"
