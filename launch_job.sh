b74226a1e363c59f0b2d6e1269840eb287f9ce7c
rtraborn <rtraborn@asu.edu>
2019-10-10 18:55:13 -0400

    Updated submission script and xdoit.

--- launch_job.sh
+++ launch_job.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-#PBS -N PdSTRIPE_job
+#PBS -N PsonSTRIPE_job
 #PBS -k o
 #PBS -l nodes=1:ppn=16,vmem=48gb
 #PBS -l walltime=8:00:00
 
 echo "Launching job"
 
-cd /N/u/rtraborn/Carbonate/ParameciumPromoters/stripe_seq/PdSTRIPE
+cd /N/u/rtraborn/Carbonate/ParameciumPromoters/stripe_seq/PsonSTRIPE
 ./xdoit > err
 
 echo "Job complete"
