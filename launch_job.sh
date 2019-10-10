1bdf1abc6dd6783ed640de2b63739344afc581f6
rtraborn <rtraborn@asu.edu>
2019-10-10 14:40:22 -0400

    Corrected file paths.

--- launch_job.sh
+++ launch_job.sh
@@ -3,12 +3,11 @@
 #PBS -N PdSTRIPE_job
 #PBS -k o
 #PBS -l nodes=1:ppn=16,vmem=48gb
-#PBS -l walltime=1:00:00
-#PBS -q debug
+#PBS -l walltime=8:00:00
 
 echo "Launching job"
 
-cd /N/u/rtraborn/Carbonate/ParameciumPromoters/stripeSeq/PdSTRIPE
+cd /N/u/rtraborn/Carbonate/ParameciumPromoters/stripe_seq/PdSTRIPE
 ./xdoit > err
 
 echo "Job complete"
