919edb41369a7dce0285f2771514f30cf29d67e6
rtraborn <rtraborn@asu.edu>
2019-10-10 16:23:44 -0400

    Added xsymlinks file and corrected paths in configfile and elsewhere.

--- launch_job.sh
+++ launch_job.sh
@@ -3,14 +3,13 @@
 #PBS -N PdodecSTRIPE_job
 #PBS -k o
 #PBS -l nodes=1:ppn=16,vmem=32gb
-#PBS -l walltime=1:00:00
-#PBS -q debug
+#PBS -l walltime=6:00:00
 
 module load java
 
 echo "Launching job"
 
-cd /N/u/rtraborn/Carbonate/ParameciumPromoters/stripeSeq/PdodecSTRIPE
+cd /N/u/rtraborn/Carbonate/ParameciumPromoters/stripe_seq/PdodecSTRIPE
 ./xdoit > err
 
 echo "Job complete"
