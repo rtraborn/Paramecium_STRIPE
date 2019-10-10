589f5e7bdc37584af8b0cf4314a1a50ec9841c2c
rtraborn <rtraborn@asu.edu>
2019-10-09 20:04:11 -0400

    Created symlinks and updated other workflow files.

--- launch_job.sh
+++ launch_job.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-#PBS -N PdSTRIPE_job
+#PBS -N PseptSTRIPE_job
 #PBS -k o
 #PBS -l nodes=1:ppn=16,vmem=48gb
-#PBS -l walltime=1:00:00
-#PBS -q debug
+#PBS -l walltime=6:00:00
 
 echo "Launching job"
 
-cd /N/u/rtraborn/Carbonate/ParameciumPromoters/stripeSeq/PdSTRIPE
+cd /N/u/rtraborn/Carbonate/ParameciumPromoters/stripe_seq/PseptSTRIPE
 ./xdoit > err
 
 echo "Job complete"
