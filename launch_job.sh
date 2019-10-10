4ecd87d53f92744b1b8136075e8ac665b212b4e0
rtraborn <rtraborn@asu.edu>
2019-10-10 16:53:53 -0400

    Updated configfile and added xsymlinks file.

--- launch_job.sh
+++ launch_job.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-#PBS -N PdSTRIPE_job
+#PBS -N PprimSTRIPE_job
 #PBS -k o
 #PBS -l nodes=1:ppn=16,vmem=48gb
-#PBS -l walltime=1:00:00
-#PBS -q debug
+#PBS -l walltime=8:00:00
 
 echo "Launching job"
 
-cd /N/u/rtraborn/Carbonate/ParameciumPromoters/stripeSeq/PdSTRIPE
+cd /N/u/rtraborn/Carbonate/ParameciumPromoters/stripe_seq/PprimSTRIPE
 ./xdoit > err
 
 echo "Job complete"
