442a6d639b5ebb08ab29fb8f35a29e47e1a57b9f
rtraborn <rtraborn@asu.edu>
2019-10-15 15:28:11 -0400

    Updated tsr-specific scripts.

---
+++ launch_job_tsrs.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#PBS -N PseptSTRIPE_job_tsrs
+#PBS -k o
+#PBS -l nodes=1:ppn=16,vmem=32gb
+#PBS -l walltime=3:00:00
+
+echo "Launching job"
+
+cd /N/u/rtraborn/Carbonate/ParameciumPromoters/stripe_seq/PseptSTRIPE
+./xdoit_tsr
+./xrunSwf > err
+
+echo "Job complete"
